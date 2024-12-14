328203ce3c8e50784d16caeedef5dff9dcd0d501
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2024-12-14 17:41:32 +0800

    test: update MockShip file

---
+++ __mocks__/MockShip.ts
@@ -0,0 +1,36 @@
+import Orientation from 'Ships/Orientation';
+
+export default class Ship {
+  protected readonly size: number;
+  protected hits = 0;
+  protected orientation: Orientation;
+
+  protected constructor(size: number, orientation: Orientation) {
+    this.size = size;
+    this.orientation = orientation;
+  }
+
+  public takesHit(): void {
+    this.hits++;
+  }
+
+  public isSunk(): boolean {
+    return this.hits >= this.size;
+  }
+
+  public getSize(): number {
+    return this.size;
+  }
+
+  public getHits(): number {
+    return this.hits;
+  }
+
+  public getOrientation(): Orientation {
+    return this.orientation;
+  }
+
+  public setOrientation(orientation: Orientation): void {
+    this.orientation = orientation;
+  }
+}
