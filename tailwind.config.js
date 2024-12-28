0d5b5bab13ce3c12bffeb9b24e7471567134f678
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2024-12-28 21:36:37 +0800

    feat: update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx,html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '425px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        battleship: "url('../assets/images/battleship.jpg')",
+      },
+    },
+    fontFamily: {
+      inter: [
+        'Inter',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
   },
   variants: {
     extend: {},
