8019d5de31d3abcaa73ecabe0bc576eabb31b889
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2025-01-01 18:59:50 +0800

    chore: remove dev tailwind script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx,html,js}'],
+  safelist: ['grid-rows-11', 'grid-cols-11'],
   theme: {
     screens: {
       xs: '425px',
